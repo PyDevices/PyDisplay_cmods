975a36aa9224a6255c8aacd8675434809c898edd
bdbarnett <brad.barnett@outlook.com>
2024-11-05 16:02:21 -0600

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-. ~/gh/esp-idf/export.sh 
-pushd ~/gh/micropython/ports/esp32
-make BOARD=ESP32_GENERIC_S3 USER_C_MODULES=../../../../busdrivers/src/micropython.cmake submodules all
+
+PORT_PATH=~/gh/micropython/ports/esp32
+BOARD=ESP32_GENERIC_S3
+C_MOD=../../../../busdrivers/src/micropython.cmake
+BUILD_FLAGS="CONFIG_FREERTOS_INTERRUPT_BACKTRACE=n"
+MANIFEST=~/gh/busdrivers/manifest.py
+IDF_PATH=~/gh/esp-idf
+
+. $IDF_PATH/export.sh
+pushd $PORT_PATH
+make -j BOARD=$BOARD clean
+make -j BOARD=$BOARD submodules
+make -j BOARD=$BOARD USER_C_MODULES=$C_MOD $BUILD_FLAGS FROZEN_MANIFEST=$MANIFEST all
+esptool --baud 460800 --erase_all write_flash 0 build-$BOARD/firmware.bin
 popd
