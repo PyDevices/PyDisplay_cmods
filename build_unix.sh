5cd1574772a1b2f0d56dc615434238135e1a6553
Brad Barnett <bbarnett@gmail.com>
2024-11-20 17:44:09 -0600

    .

--- build_unix.sh
+++ build_unix.sh
@@ -2,17 +2,19 @@
 
 GIT_DIR=$(pwd)/..
 PORT_DIR=$GIT_DIR/micropython/ports/unix
-MODULES=$GIT_DIR/lvmp
+MODULES=/home/brad/gh
 MANIFEST=$GIT_DIR/pydisplay/manifest.py
+
+BUILD_DIR=$PORT_DIR/build-standard
 
 set -e
 
 pushd $PORT_DIR
-make -j BOARD=$BOARD clean
-make -j BOARD=$BOARD submodules
-make -j BOARD=$BOARD all USER_C_MODULES=$MODULES # FROZEN_MANIFEST=$MANIFEST
+make -j clean
+make -j submodules
+make -j USER_C_MODULES=$MODULES # FROZEN_MANIFEST=$MANIFEST
 popd
 
 echo
-echo "The firmware is:  $PORT_DIR/build-standard/micropython"
+echo "The firmware is:  $BUILD_DIR/micropython"
 echo
